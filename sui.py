e383a2ffd2321160be627cd32ee15fa9eebc58fe
sui-cloud <pure199711@naver.com>
2024-08-30 23:01:55 +0900

    Update sui.py

--- sui.py
+++ sui.py
@@ -9,7 +9,7 @@ def sui():
     st.write("pandas 모듈과 Matplotlib.Seaborn 라이브러리를 활용한 데이터 시각화 orange3를 활용한") 
     st.write("데이터 분석을 배웠습니다. 오렌지3책의 프로젝트를 하나씩 해나갈 예정입니다.")
 
-sui()
+#sui()
 
 # import streamlit as st
 # def sui():
