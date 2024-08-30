7c14c5485ca7a710bc7291cbf36d47469fb2b647
sui-cloud <pure199711@naver.com>
2024-08-30 22:33:29 +0900

    Update sui.py

--- sui.py
+++ sui.py
@@ -3,19 +3,11 @@ def sui():
     st.image("초록머리앤.jpg")   
     st.write("우: 우리들은 어쩌면 만날 운명이었는지 모릅니다.")
     st.write("수: 수많은 연들이 모여서 만나진거겠죠~")
-    st.write("민: 민낯으로 인사드립니다. 수이의 첫 웹입니다. 만나서 반가워요")    
- 
-    mean  =  st.radio("우수민", ["1:우","2:수", "3:민"],index=None)
-    st.write("mean")
-    if mean=="1:우":
-        st.image("봄꽃먹기.jpg",width=150)
-        st.write("2024년 6월 18일부터 8월 29일 - '강북 여성 인력 개발 센터'")
-    elif mean=="2:수":
-        st.image("갈치한상차림.jpg",width=150)
-        st. write("AI 리터러시 전문가 과정을 수료하였습니다") 
-    elif mean=="3:민":
-        st.image("맛있게 웃어요.jpg",width=150)
-        st.write("파이썬과 오렌지를 좋아하는 '우수민' 입니다.")
+    st.write("민:(만)나서 반갑습니다. 정중히 인사드립니다. 수이의 첫 웹입니다.")    
+    st.write("2024년 6월18일부터 8월29일까지 강북여성인력개발센터 AI리터러시 포트폴리오입니다.")
+    st.write("파이썬 데이터 분석과 오렌지 데이터 분석 및 팀별 프로젝트를 수행하였습니다.")
+    st.write("pandas 모듈과 Matplotlib.Seaborn 라이브러리를 활용한 데이터 시각화 orange3를 활용한") 
+    st.write("데이터 분석을 배웠습니다. 오렌지3책의 프로젝트를 하나씩 해나갈 예정입니다.")
 
 sui()
 
